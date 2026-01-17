a777fb855565ac020bda38be2f4c66c662f0e4ab
Dose Raul <rauldose@gmail.com>
2026-01-17 18:28:12 +0100

    added syncfusion license

--- dotnet/src/DataForeman.Web/Program.cs
+++ dotnet/src/DataForeman.Web/Program.cs
@@ -38,7 +38,7 @@ builder.Services.AddAuthorizationCore();
 builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
-
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1JGaF5cXGpCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdlWX5fd3VXRGVZVkZwXUdWYEs=");
 // Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
